1daedd912667f3ab96200d6014de2e2363e509ee
Mateus707 <darkp331@gmail.com>
2024-12-13 11:55:43 -0300

    rederizando componentes com objetos de um array

--- projeto1/src/pages/index.tsx
+++ projeto1/src/pages/index.tsx
@@ -20,7 +20,32 @@ function functionDescontoTaxado(valorProduto:number,descontoProduto:number){
   return valorProduto - (descontoProduto/2);
 }
 
-
+const produtos = [
+  {
+    produto:"Mouse",
+    valor:40.00,
+    desconto:5,
+    disponivel:true
+  },
+  {
+    produto:"Teclado",
+    valor:120.00,
+    desconto:10,
+    disponivel:true
+  },
+  {
+    produto:"Monitor",
+    valor:469.99,
+    desconto:30,
+    disponivel:true
+  },
+  {
+    produto:"Cx.Som",
+    valor:209.99,
+    desconto:29.99,
+    disponivel:true
+  }
+]; 
 //função padrão home
 export default function Home() {
 
@@ -31,10 +56,13 @@ export default function Home() {
             meio
           </div>
          <div className="flex justify-center gap-3">
-            <Card produto={"Mouse"} valor={40.00} desconto={10} functionDesconto={functionDesconto}/>
-            <Card produto={"Teclado"} valor={120.00} desconto={0} functionDesconto={functionDescontoTaxado}/>
-            <Card produto={"Monitor"} valor={469.99} desconto={10} functionDesconto={functionDesconto}/>
-            <Card produto={"CPU"} valor={699.99} desconto={10} functionDesconto={functionDescontoTaxado}/>
+         {produtos.map((e:any)=>{
+                if(e.disponivel){
+                  return (
+                    <Card produto={e.produto} valor={e.valor} desconto={e.desconto} functionDesconto={functionDesconto}/>
+                  );
+                }
+            })}
          </div>
       </div>
       
