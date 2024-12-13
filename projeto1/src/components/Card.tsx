9e59e5e4c1abcf6d4bae2db2e82960d35b80f086
Mateus707 <darkp331@gmail.com>
2024-12-13 11:38:18 -0300

    Adicionando renderizações condicionais no projeto

--- projeto1/src/components/Card.tsx
+++ projeto1/src/components/Card.tsx
@@ -10,11 +10,16 @@ interface CardProps{
 
 export default function Card(props:CardProps){
     return(//passando valores via props
-        <div className="flex flex-col border border-red-600 rounded-lg p-2 ">
+        <div className={`flex flex-col border ${props.desconto>0? `border border-blue-600`:`border border-red-600`} rounded-lg p-2 `}>
             <div>Produto : {props.produto}</div>
             <div>Valor : R${props.valor}</div>
-            <div>Desconto : {props.desconto}</div>
-            <div>Valor com Desconto : {props.functionDesconto(props.valor,props.desconto)}</div>
+            {props.desconto>0 &&(
+                <div>
+                    <div>Desconto  {props.desconto}</div>
+                    <div>Valor com Desconto : {props.functionDesconto(props.valor,props.desconto)}</div>
+                </div>
+            )}
+                
         </div>
     )
 }
