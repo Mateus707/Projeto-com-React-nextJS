d756acdbb7f5717ee818fe90ae0814e9223680b0
Mateus707 <darkp331@gmail.com>
2024-12-17 13:25:57 -0300

    Passando funcoes para o componente displaystate

--- projeto1/src/components/DisplayState.tsx
+++ projeto1/src/components/DisplayState.tsx
@@ -1,9 +1,17 @@
 interface DisplayStateProps{
-    valor:number, 
+    valor:number,
+    fuctionSomar:any,
+    fuctionSubtrair:any, 
 }
 
 export default function DisplayState(props:DisplayStateProps){
     return(
-        <div>{props.valor}</div>
+        <div>
+            <div>{props.valor}</div>
+            <div className="flex justify-between items-center bg-zinc-400 h-[100px]">
+                <button className="btnPadrao" onClick={props.fuctionSomar}>Soma</button>
+                <button className="btnPadrao" onClick={props.fuctionSubtrair}>Subtrair</button>  
+            </div>
+        </div>
     )
 }
--- projeto1/src/pages/Hooks/useStates.tsx
+++ projeto1/src/pages/Hooks/useStates.tsx
@@ -3,22 +3,20 @@ import DisplayState from "@/components/DisplayState";
 
 
 
-
+ 
 export default function Usestate(){
-    const [contador,setContador] = useState<number>(0);
-      function somar(contador:number){
+
+const [contador,setContador] = useState<number>(0);
+
+    function somar(){
         setContador(contador+1);
-    }
-      function subtrair(contador:number){
+      }
+       function subtrair(){
         setContador(contador-1);
-    }
+      }
     return(
         <div>
-            <DisplayState valor={contador}/>
-            <div className="flex justify-between items-center bg-zinc-400 h-[100px]">
-                <button className="btnPadrao" onClick={()=>somar(contador)}>Soma</button>
-                <button className="btnPadrao" onClick={()=>subtrair(contador)}>Subtrair</button>  
-            </div>
+            <DisplayState valor={contador} fuctionSomar={somar} fuctionSubtrair={subtrair}/>
         </div>
     )
 }
