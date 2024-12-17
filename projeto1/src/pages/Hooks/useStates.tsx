2cfc4c0c39f245935895035fc5b2ba4cbf36f443
Mateus707 <darkp331@gmail.com>
2024-12-17 11:19:16 -0300

    passando o useStates por componentes e utilizando o valor por props

--- projeto1/src/pages/Hooks/useStates.tsx
+++ projeto1/src/pages/Hooks/useStates.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-
+import DisplayState from "@/components/DisplayState";
 
 
 
 
 export default function Usestate(){
     const [contador,setContador] = useState<number>(0);
-     function sum(contador:number){
-        return setContador(contador+1);
+      function somar(contador:number){
+        setContador(contador+1);
+    }
+      function subtrair(contador:number){
+        setContador(contador-1);
     }
     return(
         <div>
-            <div>{contador}</div>
-            <button onClick={()=>sum(contador)}>Soma</button>
+            <DisplayState valor={contador}/>
+            <div className="flex justify-between items-center bg-zinc-400 h-[100px]">
+                <button className="btnPadrao" onClick={()=>somar(contador)}>Soma</button>
+                <button className="btnPadrao" onClick={()=>subtrair(contador)}>Subtrair</button>  
+            </div>
         </div>
     )
 }
